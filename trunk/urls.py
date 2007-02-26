bd3dc1cee3e293c8f7011156facdda821dbb6881
jonlesser <jonlesser@5d1ffc85-0331-0410-89ac-950fbd9625eb>
2007-02-26 06:04:21 +0000

    Lots of changes

--- trunk/urls.py
+++ trunk/urls.py
@@ -3,9 +3,24 @@ from django.contrib.auth.views import *
 from exchtran.et.views import *
 
 urlpatterns = patterns('',
-    # Example:
-	(r'^$', et_login_form),
-
-    # Uncomment this for admin:
-	#(r'^admin/', include('django.contrib.admin.urls')),
+	# ET Pages
+	(r'^$', 'exchtran.et.views.dashboard'),
+	(r'^dashboard/$', 'exchtran.et.views.dashboard'),
+	(r'^monitor/$', 'exchtran.et.views.monitor'),
+	(r'^editor/$', 'exchtran.et.views.editor'),
+	(r'^archive/$', 'exchtran.et.views.archive'),
+	(r'^users/$', 'exchtran.et.views.users'),
+	
+	# Registration Pages
+	(r'^login/$', 'django.contrib.auth.views.login'),
+	(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login'}),
+	(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+	(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login'}),
+	(r'^accounts/profile/$', 'exchtran.et.views.profile_redirect'), # default page after login
+	
+	# Media Files
+	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/jonlesser/Documents/Berkeley/07Spring/ExchTrans/svn/exchtran/media'}),
+	
+	# Admin pages
+	(r'^admin/', include('django.contrib.admin.urls')),
 )
