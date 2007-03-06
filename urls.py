ce00ea3a9c9cff87d86908fa7d829270dc9bbf0d
jonlesser <jonlesser@5d1ffc85-0331-0410-89ac-950fbd9625eb>
2007-03-06 05:55:42 +0000

    lots of new stuff

--- urls.py
+++ urls.py
@@ -3,14 +3,21 @@ from django.contrib.auth.views import *
 from exchtran.et.views import *
 
 urlpatterns = patterns('',
-	# ET Pages
+	# ET Admin Pages
 	(r'^$', 'exchtran.et.views.dashboard'),
 	(r'^dashboard/$', 'exchtran.et.views.dashboard'),
 	(r'^monitor/$', 'exchtran.et.views.monitor'),
 	(r'^editor/$', 'exchtran.et.views.editor'),
+	(r'^editor/edit/$', 'exchtran.et.views.edit'),
+	(r'^editor/edit/addComponent/$', 'exchtran.et.views.addComponent'),	
+	(r'^editor/newExperiment/$', 'exchtran.et.views.newExperiment'),	
 	(r'^archive/$', 'exchtran.et.views.archive'),
 	(r'^users/$', 'exchtran.et.views.users'),
 	
+	# ET Experiment Pages
+	(r'^experiment/$', 'exchtran.et.views.experiment'),
+	(r'^experiment/(\d+)/$', 'exchtran.et.views.experiment'),
+		
 	# Registration Pages
 	(r'^login/$', 'django.contrib.auth.views.login'),
 	(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login'}),
