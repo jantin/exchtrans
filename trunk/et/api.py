9fc78f33eb91aff9eed0d5873999b1673db40fcc
jonlesser <jonlesser@5d1ffc85-0331-0410-89ac-950fbd9625eb>
2007-03-19 17:51:04 +0000

    Lots more stuff.

---
+++ trunk/et/api.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.template import Context, Template, RequestContext
+from et.models import *
+
+def sessionStatus(request):
+	"""docstring for sessionStatus"""
+	
+	sid = request.GET.get('sid')
+	ses = ExperimentSession.objects.get(id=sid)
+	response = ses.status.statusText
+	
+	return render_to_response('api.html', 
+							  {'response': response}, 
+							  context_instance=RequestContext(request))
+
