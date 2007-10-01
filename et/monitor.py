3f5a966ba9661810f7180262a8e9e7cb320831a4
jonlesser <jonlesser@5d1ffc85-0331-0410-89ac-950fbd9625eb>
2007-10-01 08:37:34 +0000

    Finished monitoring table. When monitoring experiments, a table of participants is dynamically updated with the participants current component.
    todo:
    -Build an interface for assigning wigets to exchange components
    -Implement formula input for X and Y value

--- et/monitor.py
+++ et/monitor.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.template import Context, Template, RequestContext
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
+from django.utils import simplejson
 from models import *
 from common import *
 import pickle
@@ -20,7 +21,7 @@ def monitor(request):
 	sid = request.GET.get('sid')
 	monitorSession = ExperimentSession.objects.get(id=sid)
 	participants = Participant.objects.filter(experimentSession=sid)
-	experimentComponents = Component.objects.filter(experimentcomponents__experiment_id__experimentsession__id=sid)
+	experimentComponents = Component.objects.filter(experimentcomponents__experiment_id__experimentsession__id=57).extra(select={'order_col': "et_component__experimentcomponents.order"}).order_by('order_col')
 	
 	if(monitorSession.status.statusText == "Running"):
 		running = True
@@ -32,7 +33,7 @@ def monitor(request):
 	else:
 		noParticipants = True
 	
-
+	print request
 	return render_to_response('adminInterface/monitor.html', 
 							{	'expSessions': expSessions, 
 								'monitorSession':monitorSession,
@@ -42,3 +43,39 @@ def monitor(request):
 								'experimentComponents':experimentComponents
 							},
 							  context_instance=RequestContext(request))
+
+@login_required
+def updatePollProcess(request):
+	"""This process returns the status of all the participants."""
+	sid = request.GET.get('sid')
+	response = []
+	
+	# Get the participants
+	participants = Participant.objects.filter(experimentSession=sid).order_by('number')
+	
+	# For each participant...
+	for p in participants:
+		pObject = {}
+		pObject['name'] = p.name
+		pObject['id'] = p.id
+		pObject['identityLetter'] = p.identityLetter
+		pObject['number'] = p.number
+		pObject['currentComponent'] = p.currentComponent
+		pObject['cumulativePoints'] = p.cumulativePoints
+		response.append(pObject)
+	
+	jsonString = simplejson.dumps(response)
+	return render_to_response('api.html', 
+						  {'response': jsonString}, 
+						  context_instance=RequestContext(request))
+
+
+@login_required
+def bootParticipant(request):
+	# TODO Don't delete, add a field to the model for inactive or something like that.
+	"""Boots participants by deleting them"""
+	pid = request.GET.get('pid')
+	p = Participant.objects.get(id=pid)
+	p.delete()
+
+	return HttpResponseRedirect(request.META['HTTP_REFERER'])
