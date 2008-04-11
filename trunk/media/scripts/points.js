5f49bf7ff27d0692e983be411e0a6af5c79733e3
n8agrin <n8agrin@5d1ffc85-0331-0410-89ac-950fbd9625eb>
2008-04-11 04:28:35 +0000

    n8: trying to fix the points table

--- trunk/media/scripts/points.js
+++ trunk/media/scripts/points.js
@@ -4,15 +4,10 @@ Points class.
 -Keeps track of the player's points throughout an exchange oppertunity
 -Calculates points preview during offer formulation
 
+n8: reverted back to version 128 because we've dropped the points table.
 */
 
-if (!PointsTable) {
-  alert('The PointsTable class must be loaded for et_points to work.');
-}
-
 function et_points(){
-  var p1Gain, p2Gain, response;
-  
 	this.p1x;
 	this.p2x;
 	this.p1y;
@@ -64,64 +59,41 @@ function et_points(){
 				response.errorMsg = "Exceeded max request";
 				return response;
 			}
-			if(parseInt(this.request) == 0 || parseInt(this.offer) == 0) {
-			  response.error = true;
-			  response.errorMsg = "You cannot give or receive 0 X's or Y's.";
-			  return response;
-			}
-			
-			// n8: 3/31/08
-			// By adding in the exchange table X's and Y's effectively become
-			// valueless.  They should not be included in the total point
-			// calculation.
 			
 			// Parse the raw values
-      // this.p1xValue = this.parseValue(this.p1xValue_raw);
-      // this.p2xValue = this.parseValue(this.p2xValue_raw);    
-      // this.p1yValue = this.parseValue(this.p1yValue_raw);    
-      // this.p2yValue = this.parseValue(this.p2yValue_raw);
+			this.p1xValue = this.parseValue(this.p1xValue_raw);
+			this.p2xValue = this.parseValue(this.p2xValue_raw);		
+			this.p1yValue = this.parseValue(this.p1yValue_raw);		
+			this.p2yValue = this.parseValue(this.p2yValue_raw);
 			
 			// Calculate the initial number of points held by the player at the start of the transaction
-			// p1InitialPoints = (this.p1x * this.p1xValue) + (this.p1y * this.p1yValue);
-			// p2InitialPoints = (this.p2x * this.p2xValue) + (this.p2y * this.p2yValue);
+			p1InitialPoints = (this.p1x * this.p1xValue) + (this.p1y * this.p1yValue);
+			p2InitialPoints = (this.p2x * this.p2xValue) + (this.p2y * this.p2yValue);
 
 			// Calculate the Gain and Loss for each player
-			var requestType = this.requestUnit.toLowerCase();
-      var offerType = this.offerUnit.toLowerCase();
-      var pointsExchange = PointsTable.findTotal(offerType, requestType, this.offer, this.request);
-      
-      // 
-      p1Gain = pointsExchange[0];
-      p2Gain = pointsExchange[1];
+			if(this.offeredBy == "p1"){
+				p1Gain = this.request * eval('this.p1'+this.requestUnit.toLowerCase()+'Value');
+				p2Gain = this.offer * eval('this.p2'+this.offerUnit.toLowerCase()+'Value');
+				p1Loss = this.offer * eval('this.p1'+this.offerUnit.toLowerCase()+'Value');
+				p2Loss = this.request * eval('this.p2'+this.requestUnit.toLowerCase()+'Value');
+			} else if(this.offeredBy == "p2") {
+				p1Gain = this.offer * eval('this.p1'+this.offerUnit.toLowerCase()+'Value');
+				p2Gain = this.request * eval('this.p2'+this.requestUnit.toLowerCase()+'Value');
+				p1Loss = this.request * eval('this.p1'+this.requestUnit.toLowerCase()+'Value');
+				p2Loss = this.offer * eval('this.p2'+this.offerUnit.toLowerCase()+'Value');
+			}
 			
-      // if(this.offeredBy == "p1" && pointsExchange){
-      //         // p1Gain = this.request * eval('this.p1'+this.requestUnit.toLowerCase()+'Value');
-      //         // p2Gain = this.offer * eval('this.p2'+this.offerUnit.toLowerCase()+'Value');
-      //         // p1Loss = this.offer * eval('this.p1'+this.offerUnit.toLowerCase()+'Value');
-      //         // p2Loss = this.request * eval('this.p2'+this.requestUnit.toLowerCase()+'Value');
-      //         p1Gain = pointsExchange[0];
-      //         p2Gain = pointsExchange[1];
-      // } else if(this.offeredBy == "p2" && pointsExchange) {
-      //         // p1Gain = this.offer * eval('this.p1'+this.offerUnit.toLowerCase()+'Value');
-      //         // p2Gain = this.request * eval('this.p2'+this.requestUnit.toLowerCase()+'Value');
-      //         // p1Loss = this.request * eval('this.p1'+this.requestUnit.toLowerCase()+'Value');
-      //         // p2Loss = this.offer * eval('this.p2'+this.offerUnit.toLowerCase()+'Value');
-      //         p1Gain = pointsExchange[1];
-      //         p2Gain = pointsExchange[0];
-      // }
+      
 
 			// calculate the potential points
-			// p1Points = p1InitialPoints + (p1Gain);
-			// p2Points = p2InitialPoints + (p2Gain);		
-			
-			p1Points = p1Gain;
-			p2Points = p2Gain;
+			p1Points = p1InitialPoints + (p1Gain - p1Loss);
+			p2Points = p2InitialPoints + (p2Gain - p2Loss);		
 
-			p1Points_justExchange = p1Gain;
-			p2Points_justExchange = p2Gain;
+			p1Points_justExchange = p1Gain - p1Loss;
+			p2Points_justExchange = p2Gain - p2Loss;
 			
-      // response.p1InitialPoints = p1InitialPoints;
-      // response.p2InitialPoints = p2InitialPoints;
+			response.p1InitialPoints = p1InitialPoints;
+			response.p2InitialPoints = p2InitialPoints;
 			response.p1Points = p1Points;
 			response.p2Points = p2Points;
 			response.p1Points_justExchange = p1Points_justExchange;
